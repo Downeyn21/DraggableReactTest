61cf54b6c2b4b89931dad72f7580eb3a18ebe9a1
Nathaniel Downey <downeyn21@gmail.com>
2025-05-31 16:22:05 -0400

    init

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+
+  draggable.array.forEach(element => {
+    
+  });
+
+  return (
+    <>
+    <div className='container'>
+      <p className='draggable' draggable='true' >Tomato</p>
+      <p className='draggable' draggable='true' >Potato</p>
+    </div>
+    <div className='container'>
+      <p className='draggable' draggable='true' >Orange</p>
+      <p className='draggable' draggable='true' >Peach</p>
+    </div>
+    </>
+  )
+}
+
+export default App
